0b1de886224e093ac2382354e83eced30a3612f1
yorkuexploree <yorkuexploree@gmail.com>
2017-02-22 20:39:16 -0500

    Hackfest Night 1, Tanniesha

---
+++ yorkExplorer/app/src/main/java/com/example/tani123/yorkexplore/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.tani123.yorkexplore;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPALSH_TIME_OUT=4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+
+        },SPALSH_TIME_OUT);
+    }
+}
